5cbae964caf479f1993c3dab7ba8bf760eb180e8
bruce <johnnybgoode@rogers.com>
2016-05-25 12:31:26 -0400

    fixed up the drop script so that everything more or less deploys to $HOME/bin
    Added some basic output to txtfix.sh, looks much better

--- txtfix.sh
+++ txtfix.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+printf "\nScrubbing bad characters in txt files of $PWD\n"
+printf  "["
 for file in *.txt; do
-	python cscrub.py "$file"
+	python $HOME/bin/py/cscrub.py "$file"
+	printf "."
 done
-
+printf "]\n"
+printf "Finished fixing characters\n\n"
 exit 0
