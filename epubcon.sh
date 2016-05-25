0d254da902d138f5dc4e2c29f4fdb85fc0bdf1fe
bruce <johnnybgoode@rogers.com>
2016-05-25 12:46:14 -0400

    added output stuff to the pdf and epub scripts

--- epubcon.sh
+++ epubcon.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+printf "\nBreaking down epub to txt in $PWD\n"
+printf "["
 for file in *.epub; do
-	ebook-convert "$file" "$file.txt"
+	ebook-convert "$file" "$file.txt" >/dev/null
+	printf "-"
 done
+printf "]\n"
+printf "Finished converting epubs to txt\n\n"
+
 
 exit 0
--- pdfcon.sh
+++ pdfcon.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+printf "\nBreaking down pdf to txt in $PWD\n"
+printf "["
 for file in *.pdf; do
-	ebook-convert "$file" "$file.txt"
+	ebook-convert "$file" "$file.txt" >/dev/null
+	printf "*"
 done
+printf "]\n"
+printf "Finished converting all pdf files to txt\n\n"
 
 exit 0
