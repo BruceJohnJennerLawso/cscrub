7175ce6649057767556e060a5afe8cbff9432ff3
bruce <johnnybgoode@rogers.com>
2016-05-25 15:04:23 -0400

    derp forgot some files
    also the epubdown.sh actually works!!!

--- epubcon.sh
+++ epubcon.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-printf "\nBreaking down epub to txt in $PWD\n"
-printf "["
-for file in *.epub; do
-	ebook-convert "$file" "$file.txt" >/dev/null
-	printf "-"
-done
-printf "]\n"
-printf "Finished converting epubs to txt\n\n"
+if [ -z "$1" ]
+	then
+		printf "\nBreaking down epub to txt in $PWD\n"
+		printf "["
+		for file in ./*.epub; do
+			ebook-convert "$file" "$file.txt" >/dev/null
+			printf "-"
+		done
+		printf "]\n"
+		printf "Finished converting epubs to txt\n\n"
 
+else
+	echo "Trying directory $1"
+	##cd $1
+	printf "\nBreaking down epub to txt in $1\n"
+	printf "["
+	for file in $1/*.epub; do
+		ebook-convert "$file" "$file.txt" >/dev/null
+		printf "-"
+	done
+	printf "]\n"
+	printf "Finished converting epubs to txt\n\n"
+fi
 
 exit 0
---
+++ epubdown.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -z "$1" ];
+	then
+		for d in *; do
+			if [[ -d "$d" ]]; then
+				echo "$d"
+				printf "$PWD/$d"
+				$HOME/bin/epubcon.sh "$d"
+				wutdownhere "$d" 1
+			fi
+		done
+else
+	cd "$1"
+	for d in *; do
+		if [[ -d "$d" ]]
+			then
+				printf "$PWD/$d"
+				$HOME/bin/epubcon.sh "$PWD/$d"
+				for i in $(seq 1 $2); do
+					printf " - "
+				done
+				printf "$d\n "
+				depth=$2
+				((depth++))
+				wutdownhere "$d" $depth
+		fi
+	done
+fi
+
+exit 0
